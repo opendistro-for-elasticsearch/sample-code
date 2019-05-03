6048fab0a7d4f4b1bce6db24cafbdd4746a152e9
Jon-AtAWS <handler@amazon.com>
2019-05-03 11:23:23 -0700

    Update main.py
    Added comments

--- pa-to-es/main.py
+++ pa-to-es/main.py
@@ -32,21 +32,30 @@ from result_parser import ResultParser
 
 
 class MetricGatherer():
+    ''' Use this class to retrieve metrics from Open Distro's Performance
+        Analyzer. Call get_all_metrics() to receive a list of ES docs. '''
 
     def __init__(self):
         pass
 
     def to_url_params(self, metric_description):
+        '''Converts a metric description into the corresponding URL params'''
         return "metrics={}&dim={}&agg={}&nodes=all".format(
             metric_description.name, ",".join(metric_description.dimensions),
             metric_description.agg)
 
     def get_metric(self, metric_description):
+        ''' Retrieves data for the metric represented by metric_description from 
+            the Performance Analyzer API. Returns the full result from the 
+            HTTP Requests library. '''
         BASE_URL = 'http://localhost:9600/_opendistro/_performanceanalyzer/metrics?'
         url = "{}{}".format(BASE_URL, self.to_url_params(metric_description))
         return requests.get(url)
 
     def get_all_metrics(self):
+        ''' Loops through all the metric descriptions, sending one at a time,
+            parsing the results, and returning a list of dicts, each one 
+            representing one future Elasticsearch document. '''
         docs = []
         for metric in metric_descriptions.get_working_metric_descriptions():
             result = self.get_metric(metric)
@@ -60,17 +69,23 @@ class MetricGatherer():
 
 
 class MetricWriter():
+    ''' Use this class to send documents in bulk to Elasticsearch'''
 
     def __init__(self, args):
+        ''' Recieves the command-line args, which must include an index root,
+            and an ES type. '''
         self.index_name = args.index_name 
         self.index_type = args.index_type
 
     def now_pst(self):
         '''Return the current time in PST timezone'''
+        ''' TODO: This should use the timezone of the current host or UTC.'''
         now_utc = datetime.now(timezone('UTC'))
         return now_utc.astimezone(timezone('US/Pacific'))
 
     def put_doc_batches(self, docs):
+        ''' Takes a list of Elasticsearch documents, interleaves the control
+            lines and sends them via the _bulk API.'''
         batch = []
         for doc in docs:
             ''' It would be better to take the index name from the doc's
@@ -93,6 +108,7 @@ class MetricWriter():
         print('Sent batch', result.status_code)
 
 def get_args():
+    ''' Parse command line arguments '''
     description = 'Send performance data from Open Distro for Elasticsearch to Elasticsearch'
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('-i', '--index-name', type=str, default='pa',
@@ -105,6 +121,20 @@ def get_args():
     return args
 
 if __name__ == '__main__':
+    ''' This is the main function for the sample code provided.
+
+            python3 main.py
+
+        to run it. You can optionally set the ES index name (a timestamp is added
+        to the --index-name, rolling over daily) and ES type via command-line.
+
+        It's a simple-minded, not-very-efficient loop that pulls all metrics
+        formats them, and pushes to Elasticsearch.
+
+        On my Mac, this loop takes ~8 seconds. Ideally it would be < 5 seconds.
+        Performance Analyzer aggregates across 5 second intervals, so this will
+        miss some data points. The obvious fix for that is to get metrics in
+        bulk, rather than one at a time.'''
     while 1:
         print('Gathering docs')
         docs = MetricGatherer().get_all_metrics()
