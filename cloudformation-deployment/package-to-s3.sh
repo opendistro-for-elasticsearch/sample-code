ea88ed808723c6f690b2f63d766a3677f4132628
Jon Handler <48454554+Jon-AtAWS@users.noreply.github.com>
2019-11-02 13:34:06 -0700

    CloudFormation templates to Deploy an Open Distro for Elasticsearch Cluster (#132)
    * Initial commit.
    
    Initial commit. Still some todo items
    
    * Add data node count and client node count to seed
    
    * Remove Kibana from the seed node
    
    * Create arch.png
    
    * Update README.md
    
    * Update README.md
    
    * Change client-nodes to deploy in an ASG

---
+++ cloudformation-deployment/package-to-s3.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+bucket=_replace_with_your_bucket_name_
+
+for file in od4es.json network.json seed.json data-nodes.json master-nodes.json client-nodes.json; do
+    echo "Sending $file"
+    aws s3 rm s3://$bucket/$file
+    aws s3 cp $file s3://$bucket/$file
+done
